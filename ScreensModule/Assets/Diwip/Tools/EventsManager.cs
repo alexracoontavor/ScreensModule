ba21086f24634181321a1e3ff0374032d051d226
Alex Tavor <alex@diwip.com>
2016-01-17 18:43:01 +0200

    Tiny cleanup

--- ScreensModule/Assets/Diwip/Tools/EventsManager.cs
+++ ScreensModule/Assets/Diwip/Tools/EventsManager.cs
@@ -1,8 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using Stego.Data;
-using System;
+﻿using System.Collections.Generic;
 
 namespace Diwip.Tools.Events
 {
