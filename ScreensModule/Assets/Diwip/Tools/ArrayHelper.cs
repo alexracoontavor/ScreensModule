957d958eeb21a0f2a19976381ecfbe178f9ef928
Alex Tavor <alex@diwip.com>
2016-02-29 16:57:32 +0200

    Added ArrayHelper

---
+++ ScreensModule/Assets/Diwip/Tools/ArrayHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class ArrayHelper
+{ 
+    public static int[] GenerateRandomIndexesArray(int length)
+    {
+        int[] answer = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            answer[i] = i;
+        }
+
+        Random rnd = new Random();
+        answer = answer.OrderBy(x => rnd.Next()).ToArray();
+
+        return answer;
+    }
+}
